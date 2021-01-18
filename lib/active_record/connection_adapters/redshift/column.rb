16bd4e34739cfecb671517dba6c9d9cd9306b9d1
Jerry Clinesmith <jerryclinesmith@gmail.com>
2021-01-18 10:41:40 -0600

    Rails 6.1:  Column - redshift does not support arrays

--- lib/active_record/connection_adapters/redshift/column.rb
+++ lib/active_record/connection_adapters/redshift/column.rb
@@ -3,6 +3,11 @@ module ActiveRecord
     class RedshiftColumn < Column #:nodoc:
       delegate :oid, :fmod, to: :sql_type_metadata
 
+      def array
+        false
+      end
+      alias :array? :array
+
     end
   end
 end
