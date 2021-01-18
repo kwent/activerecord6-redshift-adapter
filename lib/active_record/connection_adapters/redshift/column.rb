1dac198571c90a0b530ecd09242c06598651dfb9
Jerry Clinesmith <jerryclinesmith@gmail.com>
2021-01-18 10:35:50 -0600

    Rails 6.1:  Column - match initializer from ActiveRecord

--- lib/active_record/connection_adapters/redshift/column.rb
+++ lib/active_record/connection_adapters/redshift/column.rb
@@ -3,9 +3,6 @@ module ActiveRecord
     class RedshiftColumn < Column #:nodoc:
       delegate :oid, :fmod, to: :sql_type_metadata
 
-      def initialize(name, default, sql_type_metadata = nil, null = true, default_function = nil)
-        super name, default, sql_type_metadata, null, default_function
-      end
     end
   end
 end
