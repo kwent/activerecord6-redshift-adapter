88a0c1fc3e21705d65cb83bd687ce0c16b15eda5
Christian Schmidt <github@chsc.dk>
2021-03-25 10:15:18 +0100

    Add RedshiftColumn#array?

--- lib/active_record/connection_adapters/redshift/column.rb
+++ lib/active_record/connection_adapters/redshift/column.rb
@@ -3,6 +3,10 @@ module ActiveRecord
     class RedshiftColumn < Column #:nodoc:
       delegate :oid, :fmod, to: :sql_type_metadata
 
+      # Required for Rails 6.1, see https://github.com/rails/rails/pull/41756
+      mattr_reader :array, default: false
+      alias :array? :array
+
       def initialize(name, default, sql_type_metadata, null = true, default_function = nil)
         super name, default, sql_type_metadata, null, default_function
       end
